fb51b6dba8232daca54ff3ad0f38bf8c7bb79816
Maxchara <67461507+Maxchara@users.noreply.github.com>
2020-06-26 13:24:31 +0200

    Create kqdt.js

---
+++ kqdt.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login(process.env.TOKEN)
+
+bot.on('message', message => {//pingpong
+    if (message.content === ('ping') ) {
+      message.channel.send('pong !')
+    }
+    if (message.content === ('Ping')){
+      message.channel.send('Pong !')
+    }
+})
+
+
+
+
